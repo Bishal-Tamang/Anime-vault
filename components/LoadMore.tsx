33507227a4fbcb363c30982cd1f88ad8886f704d
Bishal Tamang <tbishal078@gmail.com>
2024-02-06 12:30:30 +0545

    Added framer motion for smooth transition

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -6,17 +6,20 @@ import { useEffect, useState } from "react";
 import AnimeCard from "./AnimeCard";
 
 import { useInView } from "react-intersection-observer";
-import { AnimeProp } from "./AnimeCard";
+
+export type AnimeCard = JSX.Element;
+
+let page = 2;
 
 function LoadMore() {
   const { ref, inView } = useInView();
-
-  const [data, setData] = useState<AnimeProp[]>([]);
+  const [data, setData] = useState<AnimeCard[]>([]);
 
   useEffect(() => {
     if (inView) {
-      fetchAnime(2).then((res) => {
+      fetchAnime(page).then((res) => {
         setData([...data, ...res]);
+        page++;
       });
     }
   }, [inView, data]);
@@ -24,9 +27,7 @@ function LoadMore() {
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
